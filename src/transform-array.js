e0b901fd103a21be2efdb3b1cb94affd41143fc9
Vita Kesso <buvitabu@gmail.com>
2025-11-11 02:51:42 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  let result = new Array();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-next') {
+      if (i + 1 < arr.length) {
+        result.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (result.length !== 0) {
+        result.push(arr[i - 1]);
+      }
+    } else if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      result.pop();
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = {
